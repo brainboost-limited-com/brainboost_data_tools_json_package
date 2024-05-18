f30e0fb39f83242b5b3b78a956edba26da0d387e
Pablo Tomas Borda <pablotomasborda@gmail.com>
2024-05-17 22:39:34 -0500

    package

--- setup.py
+++ setup.py
@@ -1,27 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='brainboost_data_tools_json_package',  # Replace 'my_package' with your package name
-    version='1.0.0',  # Replace with your package version
-
+    name='brainboost_data_tools_json_package',
+    version='1.0.0',
     author='Pablo Tomas Borda',
     author_email='pablotomasborda@hotmail.com',
-
-    description='Processes JSon data',
-
-    # Define your package's main content directory
-    packages=find_packages(),
-
-    # Specify package dependencies
+    description='Processes JSON data',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
     install_requires=[
-        'json',  # Example dependency with version requirement
-        'mocker'
+        'mocker',  # Example dependency
         # Add more dependencies as needed
     ],
-
-
-
-    # Optional: Define additional project URLs
     project_urls={
         'Source': 'https://github.com/your_username/my_package',  # Replace with your repository URL
     },
